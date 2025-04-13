d008cb4aabefc9c4474c60c254f7abfad727d221
Ian <Gibbu_@hotmail.com>
2025-04-13 14:18:49 +1000

    stuff

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - Sort a list in ascending order
+ * @list: Pointer to the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current;
+	listint_t *prev;
+	listint_t *tmp;
+
+	if (!list || !(*list) || !(*list)->next)
+		return;
+
+	current = (*list)->next;
+
+	while (current)
+	{
+		tmp = current;
+		current = current->next;
+
+		while (tmp->prev && tmp->n < tmp->prev->n)
+		{
+			prev = tmp->prev;
+
+			if (prev->prev)
+				prev->prev->next = tmp;
+
+			tmp->prev = prev->prev;
+			prev->next = tmp->next;
+			
+			if (tmp->next)
+				tmp->next->prev = prev;
+			
+			tmp->next = prev;
+			prev->prev = tmp;
+
+			if (!tmp->prev)
+				*list = tmp;
+
+			print_list(*list);
+		}
+	}
+}
+
